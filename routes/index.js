ae6e5b2ed8bb3f55100cf5f78c57ec430406a145
Shree Mishra <65803594+mishrashree18@users.noreply.github.com>
2021-05-04 20:56:39 +0530

    Sign in/Sign up (#14)
    * initial commit
    
    * created signin/signup
    
    * modified forgotpassword page
    
    Co-authored-by: Prateek Mohanty <42904343+StrangeNoob@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get("/", function (req, res, next) {
     res.render("pages/index", { loggedIn: false });
 });
 
+router.get("/auth", function (req, res, next) {
+    res.render("pages/auth", { loggedIn: false });
+});
+
+router.get("/forgot", function (req, res, next) {
+    res.render("pages/forgot_password", { loggedIn: false });
+});
+
+router.get("/newpassword", function (req, res, next) {
+    res.render("pages/newPassword", { loggedIn: false });
+});
+
+
+
+
+
 module.exports = router;
