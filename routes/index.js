0272ee74533aeec27aec2f7bd3d9a1f8191399a3
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2021-05-05 13:30:03 +0530

    feat:added events page

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,21 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("pages/index", { loggedIn: false });
 });
+/* Get events page*/
+router.get("/events", function (req, res, next) {
+  res.render("pages/events", { loggedIn: false });
+});
 
 router.get("/auth", function (req, res, next) {
-    res.render("pages/auth", { loggedIn: false });
+  res.render("pages/auth", { loggedIn: false });
 });
 
 router.get("/forgot", function (req, res, next) {
-    res.render("pages/forgot_password", { loggedIn: false });
+  res.render("pages/forgot_password", { loggedIn: false });
 });
 
 router.get("/newpassword", function (req, res, next) {
-    res.render("pages/newPassword", { loggedIn: false });
+  res.render("pages/newPassword", { loggedIn: false });
 });
-
-
-
-
 
 module.exports = router;
