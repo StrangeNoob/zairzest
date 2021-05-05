4fe3b06991a889f8333a2a6716d550cc08c8a4cb
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-05-05 16:18:53 +0530

    added coming soon page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get("/", function (req, res, next) {
   res.render("pages/index", { loggedIn: false });
 });
 
+/* GET coming soon page */
+router.get("/comingsoon", function (req, res, next) {
+  res.render("pages/comingsoon", { loggedIn: false });
+});
+
 module.exports = router;
