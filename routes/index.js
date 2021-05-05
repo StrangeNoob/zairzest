9cef739e06e9bc9612e6fb62814cbe9cd01584df
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-05-05 16:55:49 +0530

    resolved merge conflicts in routers/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,32 @@ const api = require("./api");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("pages/index", { loggedIn: false });
-});
+  let loggedIn = false;
+  if (req.isAuthenticated) loggedIn = true;
 
-/* GET coming soon page */
-router.get("/comingsoon", function (req, res, next) {
-  res.render("pages/comingsoon", { loggedIn: false });
+  res.render("pages/index", { loggedIn });
 });
 
 router.get("/auth", function (req, res, next) {
-  res.render("pages/auth", { loggedIn: false });
+  res.render("pages/auth");
 });
 
 router.get("/forgot", function (req, res, next) {
-  res.render("pages/forgot_password", { loggedIn: false });
+  res.render("pages/forgot_password");
 });
 
 router.get("/newpassword", function (req, res, next) {
-  res.render("pages/newPassword", { loggedIn: false });
+  res.render("pages/newPassword");
 });
 
 router.get("/profile", function (req, res, next) {
-  res.render("pages/profile", {
-    user: {
-      email: "my@eg.com",
-      name: "John Doe",
-      regdNo: "1801106333",
-      branch: "Information Technology",
-    },
-  });
+  user = req.user;
+  console.log(user);
+  res.render("pages/profile", { user });
+});
+
+router.get("/comingsoon", function (req, res, next) {
+  res.render("pages/comingsoon", { loggedIn: false });
 });
 
 router.use(api);
